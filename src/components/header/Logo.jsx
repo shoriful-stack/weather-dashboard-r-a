510ba50ef72f827739971b178407f0ecb685e7a6
Md. Shoriful Islam <sisazid182713@gmail.com>
2025-12-03 23:38:38 +0600

    feat: header component refactored

---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,8 @@
+import LogoImg from "../../assets/logo.svg"
+export default function Logo() {
+    return (
+        <a href="./index.html">
+            <img className="h-9" src={LogoImg} alt="Weather App" />
+        </a>
+    );
+}
---
+++ src/components/header/Search.jsx
@@ -0,0 +1,18 @@
+import SearchImg from "../../assets/search.svg"
+export default function Search() {
+    return (
+        <form action="#">
+            <div classname="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
+                <input
+                    classname="bg-transparent  placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
+                    type="search"
+                    placeholder="Search Location"
+                    required=""
+                />
+                <button type="submit">
+                    <img src={SearchImg} />
+                </button>
+            </div>
+        </form>
+    );
+}
